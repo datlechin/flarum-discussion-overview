f5dacd8018f3d0379165379054b8000b452294aa
Ngô Quốc Đạt <datlechin@gmail.com>
2025-02-20 09:42:17 +0700

    fix: show only actual discussion participants and refresh on discussion change
    - Fix incorrect user display by showing only actual discussion participants
    - Add proper component refresh when switching between discussions
    - Clean up previous overview instances to prevent duplicates

--- js/src/forum/components/DiscussionOverview.tsx
+++ js/src/forum/components/DiscussionOverview.tsx
@@ -1,47 +1,57 @@
 import Component from 'flarum/common/Component';
 import type Mithril from 'mithril';
 import Discussion from 'flarum/common/models/Discussion';
-import shortTime from '../helpers/shortTime';
+import User from 'flarum/common/models/User';
+import Post from 'flarum/common/models/Post';
 import Link from 'flarum/common/components/Link';
 import Tooltip from 'flarum/common/components/Tooltip';
 import avatar from 'flarum/common/helpers/avatar';
+import app from 'flarum/forum/app';
 
-export default class DiscussionOverview<Attrs> extends Component {
-  discussion: Discussion | undefined;
-  oninit(vnode: Mithril.Vnode<Attrs, this>): void {
+interface DiscussionOverviewAttrs {
+  discussion: Discussion;
+}
+
+export default class DiscussionOverview extends Component<DiscussionOverviewAttrs> {
+  discussion!: Discussion;
+
+  oninit(vnode: Mithril.Vnode<DiscussionOverviewAttrs, this>): void {
     super.oninit(vnode);
-    this.discussion = this.attrs?.discussion;
+    this.discussion = this.attrs.discussion;
   }
+
   view() {
     const discussion = this.discussion;
+    const posts = discussion.posts() as Post[];
+    const participantCount = discussion.attribute<number>('participantCount');
+    const lastPost = posts?.[posts.length - 1] as Post;
 
-    if (!discussion) {
-      return null;
-    }
-
-    const posts = discussion?.posts();
-    const participantCount = discussion?.attribute('participantCount');
-    const lastPost = posts ? posts[posts.length - 1] : null;
-    const users = Object.keys(discussion.store.data.users).map((key) => discussion.store.data.users[key]);
+    const participantUsers = new Set<User>();
+    posts.forEach((post) => {
+      const user = post.user();
+      if (user) {
+        participantUsers.add(user);
+      }
+    });
 
     return (
       <>
         <ul className="DiscussionOverview-list">
           <li className="created-at">
             <h4>{app.translator.trans('datlechin-discussion-overview.forum.created')}</h4>
-            <div className="time">{shortTime(discussion?.createdAt())}</div>
+            <div className="time">{dayjs(discussion.createdAt()).fromNow()}</div>
           </li>
           <li className="last-reply">
             <h4>{app.translator.trans('datlechin-discussion-overview.forum.last_reply')}</h4>
             <Link href={app.route.post(lastPost)}>
               <div className="time">
                 {avatar(lastPost.user())}
-                {shortTime(discussion?.lastPostedAt())}
+                {dayjs(discussion.lastPostedAt()).fromNow()}
               </div>
             </Link>
           </li>
           <li className="replies">
-            <span className="number">{discussion?.replyCount()}</span>
+            <span className="number">{discussion.replyCount()}</span>
             <h4>{app.translator.trans('datlechin-discussion-overview.forum.replies')}</h4>
           </li>
           {app.initializers.has('michaelbelgium-discussion-views') ? (
@@ -55,13 +65,13 @@ export default class DiscussionOverview<Attrs> extends Component {
             <h4>{app.translator.trans('datlechin-discussion-overview.forum.users')}</h4>
           </li>
           <li className="likes">
-            <span className="number">{posts[0].likes().length}</span>
+            <span className="number">{posts[0].attribute('likesCount') || 0}</span>
             <h4>{app.translator.trans('datlechin-discussion-overview.forum.likes')}</h4>
           </li>
           <li className="avatars">
             <div className="user-list">
-              {users.map((user) => (
-                <Tooltip text={user?.attribute('username')}>
+              {Array.from(participantUsers).map((user: User) => (
+                <Tooltip key={user.id()} text={user.attribute('username')}>
                   <Link href={app.route.user(user)}>{avatar(user)}</Link>
                 </Tooltip>
               ))}
